1ff443a6eb426f5ca5beb19b6c4626b999a45f75
jasonkim7288 <jason.kim7288@gmail.com>
2020-08-31 10:53:59 +1000

    deploy to netlify

---
+++ build/precache-manifest.7b49d191d4efb909fee1c6b99dfa5792.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93dedacf948e8e2fb3ae5f552ddc264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cb3b1eb2869f05e8963",
+    "url": "/static/css/main.75a8f5db.chunk.css"
+  },
+  {
+    "revision": "15c7c643cce34b5dab31",
+    "url": "/static/js/2.234182ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.234182ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb3b1eb2869f05e8963",
+    "url": "/static/js/main.1bdbee47.chunk.js"
+  },
+  {
+    "revision": "a9f5bc2a15d1cea6a461",
+    "url": "/static/js/runtime-main.293f41c3.js"
+  }
+]);
